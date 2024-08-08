c78428b929a471173b97f7943878bce6d428aa0c
Ebrahim Ahmed <ibrahim9331st@gmail.com>
2024-08-08 06:03:50 -0400

    Day1Exercises

---
+++ Day1/main1.py
@@ -0,0 +1,38 @@
+
+# Exercise 1
+UserString = input("Say what you want: ")
+print(UserString)
+
+# Exercise 2
+userNo = int(input("Enter a number please: "))
+userNo += 1
+print(int(userNo))
+
+# Exercise 3
+user_no = float(input("Enter another Number: "))
+user_no += .5
+print(float(user_no))
+
+# Exercise 4
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+Totalno = float(num1) + float(num2)
+
+print("The sum is:", Totalno)
+
+# Exercise 5
+no_1 = input("Enter A first #: ")
+no_2 = input("Enter A second #: ")
+
+total_2 = float(no_1) * float(no_2)
+print(total_2)
+
+# Exercise 6
+# If we enter a decimal number here, we will get an error when we run it.
+x = int(input("Enter the first int number: "))
+y = int(input("Enter the second int number: "))
+
+result = x / y
+
+print(result)
